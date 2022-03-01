27205fe20514235d9d1be7e3f37615cac210e21a
dcnis <bgk.dennis@yahoo.de>
2022-03-01 12:45:53 +0800

    added default spring project

---
+++ src/test/java/de/dcnis/dubbo/DubboApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dcnis.dubbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
